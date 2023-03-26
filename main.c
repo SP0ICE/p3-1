c5813c85324a1278aee54f236411e15bb2df754b
ilya knyazev <ilak9081@gmail.com>
2023-03-26 12:30:32 +0400

    p3-1

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+#define katet_a 10  
+#define katet_b 15
+int main() {
+float area;
+area=(katet_a*katet_b)/2;
+printf("The area of the triangle is ""%f",area);
+}
